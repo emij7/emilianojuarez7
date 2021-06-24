0dce6e9daf899cd085d2de552a1fbad1b99d6d79
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-06-23 23:21:52 -0300

    Test

---
+++ src/pages/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HelloWorld = ()=>{
+    return(
+        <div>
+                    <h1>Hello world</h1>
+                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet molestiae delectus necessitatibus. Dolor ipsam est fugiat consectetur quisquam corrupti alias nihil porro quia. Vitae, incidunt? Nulla voluptate autem laboriosam quos!Z</p>
+        </div>
+
+    )
+}
+
+export default HelloWorld
