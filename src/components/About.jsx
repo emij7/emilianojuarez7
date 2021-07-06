4fc02b534b3ad5eb52d3ef22b60d694c599366c6
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-07-05 23:45:22 -0300

    Navbar responsive. Added style to Title section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import useWidth from '../hooks/useWidth'
+import myPic from '../images/myPic.jpg'
 
-const About = () =>{
+const About = () =>{    
+    let currentWidth = useWidth()
     return(
-        <h1>holi</h1>
+        <div>
+            <div className='title'>
+                <h1>¡Hola!</h1>
+                <h2>Mi nombre es Ignacio Emiliano Juárez y esta es mi página personal.</h2>
+                <figure className='title__figure'>
+                    <img src={myPic} alt="Photograph of Ignacio holding a trophy" />
+                    <figcaption>Yo sosteniendo un trofeo de Handball.</figcaption>
+                </figure>
+            </div>
+            <section id='about' className='about'>
+                <p>Soy <span className='about--highlight-text'>desarrollador Front-end</span>, utilizo las tecnologías HTML, CSS, JAVASCRIPT, GITHUB, SASS y REACT.JS.</p>
+                <p>Soy auto-didacta. Estudio en Platzi, en FreeCodeCamp, leo documentación oficial y veo artículos que encuentro on-line.</p>
+                {currentWidth>767?
+                <div>
+                    <p>Tengo mucho para ofrecer, mientras trabajo sigo estudiando distintas tecnologías de desarrollo, estoy mejorando mi inglés para que sea mi segundo lenguaje, soy responsable con las tareas que me son asignadas y creo en que el trabajo en equipo es la forma adecuada de conseguir metas.</p>
+                    <p>He llegado al mundo de la programación gracias a un amigo, desde que supe que existía la programación supe que esto es lo que quiero hacer. Me satisface mucho resolver problemas y poder crear contenido.</p>
+                    <p>A vos que está del otro lado de la pantalla, sientete libre de contactarme por la razón que sea. En este momento estoy buscando un trabajo fijo como desarrollador pero también puedes escribirme para hablar sobre lenguaje o para darme feedback sobre mi web y con gusto te respondo. Escribime a mi correo: <span className='about--highlight-text'>juarezignacioemiliano@gmail.com</span> o encuentrame en otras redes que se encuentran abajo al final de la página.
+                </p>
+                </div>
+                :null}
+                <p>Mas abajo hay más sobre mí, mis certificados y proyectos.</p>
+                <p className='about__thanks'>!Muchas gracias por pasar!</p>
+            </section>
+        </div>
     )
 }
 export default About
--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -1,12 +1,14 @@
 import React,{useState} from 'react'
 import menuIcon from '../images/menu.png'
 import cancel from '../images/cancel.png'
+import useWidth from '../hooks/useWidth';
 
 const NavbarMenu = () =>{
     const [menu,setMenu] = useState(false);
     const handleClick = ()=>{
         setMenu(!menu)
     }
+    let currentWidth = useWidth()
     return(
         <div className='navbar__links'>
             <button className='navbar__menu-icon' onClick={handleClick}>
@@ -14,11 +16,16 @@ const NavbarMenu = () =>{
                     <img src={menu?cancel:menuIcon} alt="Deploy menu icon" />
                 </div>
             </button>
-            <div className={!menu?'navbar__links--disappear':'navbar__links--appear'}>
-                <a href="/#">Sobre mi</a>
-                <a href="/#">Certificados</a>
-                <a href="/#">Proyectos</a>
-                <a href="/#">Contacto</a>
+            <div className={currentWidth<768?
+             menu?
+                    'navbar__links--appear'
+                    :'navbar__links--empty'
+            :'navbar__links--row'
+            }>
+                <a href="#about" onClick={handleClick}>SOBRE MI</a>
+                <a href="/#" onClick={handleClick}>CERTIFICADOS</a>
+                <a href="/#" onClick={handleClick}>PROYECTOS</a>
+                <a href="/#" onClick={handleClick}>CONTACTO</a>
             </div>
         </div>
     )
