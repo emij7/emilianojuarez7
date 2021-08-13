20a2e184dd051e89f89a7900ef10fcc7062e2e5e
Ignacio Emiliano Juarez <75501977+emij7@users.noreply.github.com>
2021-08-13 11:24:26 -0300

    Updated about section with a better description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,19 +8,19 @@ const About = () =>{
         <div className='about__container'>
             <div className='title'>
                 <h1>¡HOLA!</h1>
-                <h2>Mi nombre es Ignacio Emiliano Juárez y esta es mi página personal. <br />(Actualmente en actualización)</h2>
+                <h2>Mi nombre es Ignacio Emiliano Juárez y esta es mi página personal.</h2>
                 <figure className='title__figure'>
                     <img src={myPic} alt="Photograph of Ignacio holding a trophy" />
                     <figcaption>Premiación de Handball en San Luis. 2018.</figcaption>
                 </figure>
             </div>
             <section id='about' className='about'>
-                <p>Soy <span className='about--highlight-text'>desarrollador Front-end</span>, utilizo las tecnologías HTML, CSS, JAVASCRIPT, GITHUB, SASS y REACT.JS.</p>
+                <p>Soy <span className='about--highlight-text'>desarrollador Front-end</span>, para crear esta página utilicé las tecnologías HTML, CSS, JAVASCRIPT, GIT, SASS y REACT.JS.</p>
                 <p>Soy auto-didacta. Estudio en Platzi, en FreeCodeCamp, leo documentación oficial y veo artículos que encuentro on-line.</p>
                 {currentWidth>767?
                 <div>
-                    <p>Tengo mucho para ofrecer, mientras trabajo sigo estudiando distintas tecnologías de desarrollo, estoy mejorando mi inglés para que sea mi segundo lenguaje, soy responsable con las tareas que me son asignadas y creo en que el trabajo en equipo es la forma adecuada de conseguir metas.</p>
-                    <p>He llegado al mundo de la programación gracias a un amigo, desde que supe que existía la programación supe que esto es lo que quiero hacer. Me satisface mucho resolver problemas y poder crear contenido.</p>
+                    <p>Mientras trabajo sigo estudiando distintas tecnologías de desarrollo, estoy mejorando mi inglés para que sea mi segundo lenguaje, soy responsable con las tareas que me son asignadas y creo en que el trabajo en equipo es la forma adecuada de conseguir metas.</p>
+                    <p>Desde que supe que existía la programación supe que esto es lo que quiero hacer. Me satisface mucho resolver problemas y poder crear contenido.</p>
                 </div>
                 :null}
                 <p>Mas abajo hay más sobre mí, mis certificados y proyectos.</p>
